c903aabdb054bb95840496d24892ffcf68fe3712
Tomasz Pasternak <tomasz.pasternak@euvic.pl>
2022-08-01 14:32:30 +0200

    added popular-movies.components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PopularMoviesComponent } from './popular-movies/popular-movies.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PopularMoviesComponent
   ],
   imports: [
     BrowserModule,
