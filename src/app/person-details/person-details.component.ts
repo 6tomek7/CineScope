32e78b452266c487ff30282ce0572538063d1ed4
Tomasz Pasternak <tomasz.pasternak@euvic.pl>
2022-08-11 10:30:34 +0200

    implementation person-details

--- src/app/person-details/person-details.component.ts
+++ src/app/person-details/person-details.component.ts
@@ -1,4 +1,9 @@
+import { environment } from './../../environments/environment';
+import { Observable } from 'rxjs/internal/Observable';
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Person } from '../movies.service';
 
 @Component({
   selector: 'app-person-details',
@@ -6,9 +11,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./person-details.component.css']
 })
 export class PersonDetailsComponent implements OnInit {
+  urlPersonDetails = environment.urlPersonDetails
+  apiKey = environment.apiKey
+  urlImage = environment.urlImage
+  private _id: any
+  person$!: Observable<Person>
 
-  constructor() { }
+  constructor(
+    private route: ActivatedRoute,
+    private http: HttpClient
+  ) { }
 
   ngOnInit(): void {
+    this._id = this.route.snapshot.params["id"] 
+    this.person$ = this.http.get<Person>( this.urlPersonDetails+this._id+this.apiKey);
   }
 }
