965a4ea1e946765ca16b9a874bffad87838fdaf5
Tomasz Pasternak <tomasz.pasternak@euvic.pl>
2022-08-12 09:33:39 +0200

    added  urlSearchGenres

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -9,5 +9,7 @@ export const environment = {
   urlSearchMovies: "https://api.themoviedb.org/3/search/movie?api_key=38193385b589296926c46f16b67e1b93&language=en-US&query=",
   urlCast: "https://api.themoviedb.org/3/movie/",
   urlCredits: "/credits",
-  urlPersonDetails: "https://api.themoviedb.org/3/person/"
+  urlPersonDetails: "https://api.themoviedb.org/3/person/",
+  urlSearchGenres: "https://api.themoviedb.org/3/discover/movie?api_key=38193385b589296926c46f16b67e1b93&sort_by=popularity.desc&page=1&with_genres="
+  /* List of the most popular films of the selected genre */
 };
