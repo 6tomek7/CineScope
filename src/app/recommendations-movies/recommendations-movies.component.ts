6d0d5b33a2ef7e21ca960df74c3c9c182e8487b1
Tomasz Pasternak <tomasz.pasternak@euvic.pl>
2022-08-17 08:46:44 +0200

    adding to movie-details.component the list of recommended films

--- src/app/recommendations-movies/recommendations-movies.component.ts
+++ src/app/recommendations-movies/recommendations-movies.component.ts
@@ -1,4 +1,9 @@
+import { Recommendations, RecommendationsResult } from './../movies.service';
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { environment } from 'src/environments/environment';
+import { Observable } from 'rxjs/internal/Observable';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-recommendations-movies',
@@ -6,10 +11,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recommendations-movies.component.css']
 })
 export class RecommendationsMoviesComponent implements OnInit {
-
-  constructor() { }
+  urlImage = environment.urlImage
+  private _id: string | undefined
+  constructor(
+    private route: ActivatedRoute,
+    private http: HttpClient) { }
+  movies: Array<RecommendationsResult> | undefined;
+  data$!: Observable<Recommendations>
 
   ngOnInit(): void {
+    this._id = this.route.snapshot.params["id"]
+    this.data$ = this.http.get<Recommendations>(`${environment.apiUrl}/movie/${this._id}/recommendations${environment.apiKey}`)
   }
-
 }
