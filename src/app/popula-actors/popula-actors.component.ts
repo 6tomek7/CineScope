e93afeda6178d6f69b6a89ab8af87fbf977ceac3
Tomasz Pasternak <tomasz.pasternak@euvic.pl>
2022-09-16 15:17:58 +0200

    added popula-actors components

---
+++ src/app/popula-actors/popula-actors.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-popula-actors',
+  templateUrl: './popula-actors.component.html',
+  styleUrls: ['./popula-actors.component.css']
+})
+export class PopulaActorsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
