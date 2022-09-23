b6111fb2601685ccbbebab336793c4fae4ea7c7e
Tomasz Pasternak <tomasz.pasternak@euvic.pl>
2022-09-23 15:20:51 +0200

    added to supportForPopularComponent opening methods app-popula-actors, app-popular-on-tv, app-popular-movies

--- src/app/support-for-popular/support-for-popular.component.ts
+++ src/app/support-for-popular/support-for-popular.component.ts
@@ -6,10 +6,31 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./support-for-popular.component.css']
 })
 export class SupportForPopularComponent implements OnInit {
+  showMovies = false
+  showActors = false
+  showOnTv = false
 
   constructor() { }
 
   ngOnInit(): void {
+    this.viewMovies()
   }
 
+  viewMovies(){
+    this.showMovies = !this.showMovies
+    this.showActors = false
+    this.showOnTv = false
+  }
+
+  viewActors(){
+    this.showActors = !this.showActors
+    this.showMovies = false
+    this.showOnTv= false
+  }
+
+  viewOnTv(){
+    this.showOnTv = !this.showOnTv
+    this.showMovies = false
+    this.showActors = false
+  }
 }
