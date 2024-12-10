21fd9376c387eb18d6c01f43f4bd33817d33f791
Tom <krzysiekwichura@gmail.com>
2024-12-10 19:20:21 +0100

    removed unnecessary code

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,7 @@
-import { Component, isDevMode } from '@angular/core';
+import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
-  constructor() {
-    if (isDevMode()) {
-      console.log('Development!');
-    } else {
-      console.log('Production!');
-    }
-  }
-}
+export class AppComponent {}
